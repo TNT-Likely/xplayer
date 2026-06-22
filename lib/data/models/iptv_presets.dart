4653e6432df7c369848e9ae8aff94140e10f3ff4
sunxiao <sunxiaoyes@outlook.com>
2026-06-23 00:32:32 +0800

    feat: 推荐源扩充(全部/港/台/新/美/英/日/韩 + 体育/新闻/电影,均已验证上游存在)

--- lib/data/models/iptv_presets.dart
+++ lib/data/models/iptv_presets.dart
@@ -23,29 +23,27 @@ class IptvPreset {
   });
 }
 
+const String _base = 'https://iptv-org.github.io/iptv';
+
 /// 默认预置源：iptv-org 中国（首启自动加载）。
 const IptvPreset kDefaultPreset = IptvPreset(
   nameKey: 'presetChina',
-  fallbackName: '中国 · iptv-org',
-  url: 'https://iptv-org.github.io/iptv/countries/cn.m3u',
+  fallbackName: '中国 China',
+  url: '$_base/countries/cn.m3u',
 );
 
-/// 可在「推荐源」里一键添加的预置列表。
+/// 「推荐源」里可一键添加的预置（国家 + 全部 + 分类，均已确认上游存在）。
 const List<IptvPreset> kIptvPresets = [
   kDefaultPreset,
-  IptvPreset(
-    nameKey: 'presetSports',
-    fallbackName: '体育 · iptv-org',
-    url: 'https://iptv-org.github.io/iptv/categories/sports.m3u',
-  ),
-  IptvPreset(
-    nameKey: 'presetNews',
-    fallbackName: '新闻 · iptv-org',
-    url: 'https://iptv-org.github.io/iptv/categories/news.m3u',
-  ),
-  IptvPreset(
-    nameKey: 'presetAll',
-    fallbackName: '全部 · iptv-org',
-    url: 'https://iptv-org.github.io/iptv/index.m3u',
-  ),
+  IptvPreset(nameKey: 'presetAll', fallbackName: '全部 All', url: '$_base/index.m3u'),
+  IptvPreset(nameKey: 'presetHK', fallbackName: '香港 Hong Kong', url: '$_base/countries/hk.m3u'),
+  IptvPreset(nameKey: 'presetTW', fallbackName: '台湾 Taiwan', url: '$_base/countries/tw.m3u'),
+  IptvPreset(nameKey: 'presetSG', fallbackName: '新加坡 Singapore', url: '$_base/countries/sg.m3u'),
+  IptvPreset(nameKey: 'presetUS', fallbackName: '美国 United States', url: '$_base/countries/us.m3u'),
+  IptvPreset(nameKey: 'presetUK', fallbackName: '英国 United Kingdom', url: '$_base/countries/uk.m3u'),
+  IptvPreset(nameKey: 'presetJP', fallbackName: '日本 Japan', url: '$_base/countries/jp.m3u'),
+  IptvPreset(nameKey: 'presetKR', fallbackName: '韩国 Korea', url: '$_base/countries/kr.m3u'),
+  IptvPreset(nameKey: 'presetSports', fallbackName: '体育 Sports', url: '$_base/categories/sports.m3u'),
+  IptvPreset(nameKey: 'presetNews', fallbackName: '新闻 News', url: '$_base/categories/news.m3u'),
+  IptvPreset(nameKey: 'presetMovies', fallbackName: '电影 Movies', url: '$_base/categories/movies.m3u'),
 ];
